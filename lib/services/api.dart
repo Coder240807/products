a650262d08ef2ea5782bed1e8a23d0ac2f6eb71b
Coder240807 <verma.vigya2007@gmail.com>
2026-09-22 16:29:56 +0530

    first commit

---
+++ lib/services/api.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:products/models/product.dart';
+
+class Api {
+  Future<List<Product>> getProducts() async {
+    final response = await http.get(
+      Uri.parse('https://dummyjson.com/products'),
+    );
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = jsonDecode(response.body);
+      final List list = data['products'];
+      return list.map((item) => Product.fromJson(item)).toList();
+    }
+    throw Exception('Error');
+  }
+}
